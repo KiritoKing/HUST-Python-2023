a08104cf48ae9d889cb83ecdf7e97e146be45f72
KiritoKing <kiritoclzh@gmail.com>
2023-05-10 23:06:58 +0800

    try: 尝试修改服务端charset

--- app/utils/server.py
+++ app/utils/server.py
@@ -1,14 +1,28 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler as RH
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 from functools import partial
+import urllib.parse
 import threading
+
+class Utf8HTTPRequestHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        # 设置响应头的Content-Type，指定编码为utf-8
+        self.send_header('Content-Type', 'text/html; charset=utf-8')
+        super().end_headers()
+
+    def translate_path(self, path):
+        # 解码请求路径中的特殊字符
+        path = path.encode('utf-8')
+        path = urllib.parse.unquote(path)
+        # 调用父类的方法，获取处理后的路径
+        translated_path = super().translate_path(path)
+        return translated_path
 
 
 class Server(threading.Thread):
     def __init__(self, port: int, path: str = "shared") -> None:
         super().__init__()
         self.dir = path
-        RH.extensions_map = {k: v + ';charset=UTF-8' for k, v in RH.extensions_map.items()}
-        handler = partial(RH, directory=path)
+        handler = partial(Utf8HTTPRequestHandler, directory=path)
         self.server = HTTPServer(('0.0.0.0', port), handler)
         self.runing = False
 
